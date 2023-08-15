84f9f4ebda489b18fa82ec3974453f3c302553f0
cy1der <mrvenomousgd@gmail.com>
2023-08-14 21:33:43 -0400

    i gotchu

--- data-collect/src/utils.rs
+++ data-collect/src/utils.rs
@@ -1,6 +1,6 @@
 use crate::config::{BASE_URL, KEYS, NUM_KEYS};
 use chrono::{prelude::*, ParseError};
-use csv::{Reader, ReaderBuilder, StringRecord, Writer};
+use csv::{Reader, ReaderBuilder, StringRecord, StringRecordIter, StringRecordsIter, Writer};
 use reqwest::{Client, Error, Request};
 use serde_json::Value;
 use std::{
@@ -13,28 +13,27 @@ use std::{
     time::Duration,
 };
 
-
-
-
 pub fn filter_csv() {
     let data_path: &Path = Path::new("data/merged_data.csv");
 
-
-    //let entry = read_dir(data_path).expect("Failed to get file entry");
-
     if data_path.is_file() && data_path.extension().unwrap_or_default() == "csv" {
-        let file: File = File::open(&data_path).expect("Failed to open file");
+        let file: File = File::open(data_path).expect("Failed to open file");
         let mut reader: Reader<File> = ReaderBuilder::new().has_headers(false).from_reader(file);
+        let data: Vec<Vec<String>> = reader
+            .records()
+            .map(|record| {
+                record
+                    .expect("Failed to get result")
+                    .iter()
+                    .map(|field| field.to_string())
+                    .collect()
+            })
+            .collect::<Vec<Vec<String>>>();
 
-        if let Some(max_columns_row) = reader.records().into_iter().max_by_key(|row| row.unwrap().len()) {
-            for result in reader.records() {
-                let record: StringRecord = result.expect("Failed to get csv record");
-                if record.len() != max_columns_row.unwrap().clone().len() {
-            
-                }
-            }
-            
-        }
-        
+        let longest_row_length: usize = data
+            .iter()
+            .max_by_key(|row| row.len())
+            .expect("Failed to get longest row")
+            .len();
     }
 }
