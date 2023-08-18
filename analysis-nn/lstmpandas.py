a119f4ae019c6cf96b98bb93392eea97590d1787
xv786vx <firas.aj@icloud.com>
2023-08-17 22:25:38 -0400

    might be getting smwhere

--- analysis-nn/lstmpandas.py
+++ analysis-nn/lstmpandas.py
@@ -43,25 +43,21 @@ df = pd.read_csv(os.path.join(os.getcwd(), "data-collect", "data", "merged_norma
 # print(df.head(5))
 print(df.info())
 
-# y = df.iloc[:, 10]
-# X = df.drop(df.columns[10], axis=1)
+y = np.array(df.iloc[:, 10]).astype('float32')
+X = df.drop(df.columns[10], axis=1)
+X = np.array(df.drop(df.columns[0], axis=1)).astype('float32')
 
-# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# X_train = float(X_train)
-# X_test = float(X_test)
-# y_train = float(y_train)
-# y_test = float(y_test)
+X_train = X_train.reshape((X_train.shape[0], 1, X_train.shape[1]))
+X_test = X_test.reshape((X_test.shape[0], 1, X_test.shape[1]))
 
-# X_train = X_train.values.reshape((X_train.shape[0], 1, X_train.shape[1]))
-# X_test = X_test.values.reshape((X_test.shape[0], 1, X_test.shape[1]))
+num_features = X_train.shape[2]
 
-# num_features = X_train.shape[2]
+model = Sequential()
+model.add(LSTM(50, return_sequences=True, input_shape=(1, num_features)))
+model.add(Dense(1))
+model.compile(optimizer='adam', loss='mean_squared_error')
 
-# model = Sequential()
-# model.add(LSTM(50, return_sequences=True, input_shape=(1, num_features)))
-# model.add(Dense(1))
-# model.compile(optimizer='adam', loss='mean_squared_error')
-
-# model.fit(X_train, y_train, epochs=50, batch_size=32, verbose=0)
-# y_pred = model.predict(X_test)
+model.fit(X_train, y_train, epochs=50, batch_size=32, verbose=0)
+y_pred = model.predict(X_test)
